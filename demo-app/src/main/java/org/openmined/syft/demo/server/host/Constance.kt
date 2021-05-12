ddaf87c69ea3b706d585a0d46254af39893558ff
VihangaDehigaspitiya <rashmikarc@gmail.com>
2021-05-13 00:07:24 +0530

    added production api url

--- demo-app/src/main/java/org/openmined/syft/demo/server/host/Constance.kt
+++ demo-app/src/main/java/org/openmined/syft/demo/server/host/Constance.kt
@@ -2,7 +2,7 @@ package org.openmined.syft.demo.server.host
 
 object Constants {
 
-    const val BASE_URL = "http://192.168.1.5:4000"
+    const val BASE_URL = "http://139.59.77.149"
     const val LOGIN_URL = "user/login"
     const val POSTS_URL = "posts"
 
